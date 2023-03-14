d94297e1a4ac69084fdfd6cbb664ab6285edec2d
sbegic <sbegic2003@gmail.com>
2023-03-14 11:31:54 +0100

    [XLS-Import] made a html file, that displays the 2 buttons for importing files. (04:55)

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { XlsImportComponent } from './xls-import/xls-import.component';
+import { UploadButtonComponent } from './upload-button/upload-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XlsImportComponent,
+    UploadButtonComponent
   ],
   imports: [
     BrowserModule
