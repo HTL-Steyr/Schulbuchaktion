11b47900c7ed8bcb7f36faab47cf35545c39bbe7
lukas <lnagler1@htl-steyr.ac.at>
2023-03-14 10:44:34 +0100

    [TASK] devextreme (00:05)

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {DxButtonModule, DxDataGridModule} from 'devextreme-angular';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxButtonModule,
+    DxDataGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
