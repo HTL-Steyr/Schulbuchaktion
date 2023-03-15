120fd16ed1c611959bc7e8857ee5bf90b0796adf
s-prechtl <stefan@tague.at>
2023-03-15 05:58:23 +0100

    [TASK] Added a little not found page for unsupported routes (00:05h)

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { OrderlistComponent } from './orderlist/orderlist.component';
 
 const routes: Routes = [
     {path: '', redirectTo: '/bestelluebersicht', pathMatch:'full'},
     {path: 'bestelluebersicht', component: OrderlistComponent},
+    {path: '**', component: NotFoundComponent}
 ]
 
 @NgModule({
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import {DxButtonModule, DxDataGridModule} from 'devextreme-angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { OrderlistComponent } from './orderlist/orderlist.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OrderlistComponent
+    OrderlistComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
