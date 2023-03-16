98f698535737a6517577fdd1e18b0babc8a06b51
Iris <istoeckl@htblasteyr.onmicrosoft.com>
2023-03-16 11:12:34 +0100

    [TASK] RoutingComponent Classes (02:00h)

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { OrderlistComponent } from './orderlist/orderlist.component';
+import {ClassesOverviewComponent} from "./classes-overview/classes-overview.component";
 
 const routes: Routes = [
     {path: '', redirectTo: '/bestelluebersicht', pathMatch:'full'},
     {path: 'bestelluebersicht', component: OrderlistComponent},
-    {path: '**', component: NotFoundComponent}
+  {path: 'klassenuebersicht', component:ClassesOverviewComponent},
+  {path: '**', component: NotFoundComponent}, //LEAVE THIS LAST!
 ]
 
 @NgModule({
