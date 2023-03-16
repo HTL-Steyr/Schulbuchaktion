bde7b2d0604367a5250639c29ba8b775959bcc00
smedziko <smedziko@htl-steyr.ac.at>
2023-03-16 10:37:12 +0100

    [TASK] Login-Page / LoginComponent (00:40)

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { OrderlistComponent } from './orderlist/orderlist.component';
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
     {path: '', redirectTo: '/bestelluebersicht', pathMatch:'full'},
     {path: 'bestelluebersicht', component: OrderlistComponent},
-    {path: '**', component: NotFoundComponent}
+    {path: 'login', component: LoginComponent},
+    {path: '**', component: NotFoundComponent},
 ]
 
 @NgModule({
