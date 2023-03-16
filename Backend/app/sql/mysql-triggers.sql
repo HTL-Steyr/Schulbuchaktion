0d9b33a62d981b22c9c0046d1bee50d279d518b0
decker213 <decker@htl-steyr.ac.at>
2023-03-16 14:52:24 +0100

    [TASK] create mysql-triggers.sql to calculate price for book_order after new order was created. Update used_budget for school_class and department (01:30h)

---
+++ Backend/app/sql/mysql-triggers.sql
@@ -0,0 +1,50 @@
+CREATE TRIGGER t_create_book_order
+    AFTER INSERT
+    ON book_order
+    FOR EACH ROW
+BEGIN
+    DECLARE pricePerBook INTEGER;
+    DECLARE totalPrice INTEGER;
+    DECLARE schoolClassId INTEGER;
+    DECLARE departmentId INTEGER;
+
+    SELECT school_class_id_id FROM book_order INTO schoolClassId;
+    SELECT department_id_id FROM school_class WHERE school_class.id = schoolClassId INTO departmentId;
+
+    IF (SELECT ebook_plus FROM book_order)
+    THEN
+    SELECT price_ebook_plus + (price_inclusive_ebook - book_price.price_ebook)
+    FROM book_price
+    WHERE book_price.book_id_id = (SELECT book_order.book_id_id FROM book_order WHERE id = last_insert_id())
+        INTO pricePerBook;
+    ELSE
+        IF (SELECT ebook FROM book_order)
+        THEN
+    SELECT price_inclusive_ebook
+    FROM book_price
+    WHERE book_price.book_id_id = (SELECT book_order.book_id_id FROM book_order WHERE id = last_insert_id())
+        INTO pricePerBook;
+    ELSE
+    SELECT price_inclusive_ebook - book_price.price_ebook
+    FROM book_price
+    WHERE book_price.book_id_id = (SELECT book_order.book_id_id FROM book_order WHERE id = last_insert_id())
+        INTO pricePerBook;
+END IF;
+END IF;
+
+    SET totalPrice = pricePerBook * (SELECT count FROM book_order WHERE id = last_insert_id());
+
+UPDATE book_order SET price=totalPrice WHERE id = last_insert_id();
+
+UPDATE school_class
+SET used_budget=(totalPrice + (SELECT used_budget
+                               FROM school_class
+                               WHERE school_class.id = schoolClassId))
+WHERE id = schoolClassId;
+
+UPDATE department
+SET used_budget=(totalPrice + (SELECT used_budget
+                               FROM department
+                               WHERE department.id = departmentId))
+WHERE id = departmentId;
+END;
