9db0a88de642057002fd8d4f0aaf29efcabdd1a0
simon <skimeswe@htl-steyr.ac.at>
2023-03-21 09:05:19 +0100

    [<TASK>] <Testdatensätze> (<01:00>h)

--- Backend/app/sql/mysql-triggers.sql
+++ Backend/app/sql/mysql-triggers.sql
@@ -11,30 +11,35 @@ BEGIN
     SELECT school_class_id_id FROM book_order INTO schoolClassId;
     SELECT department_id_id FROM school_class WHERE school_class.id = schoolClassId INTO departmentId;
 
-    IF (SELECT ebook_plus FROM book_order)
+    IF (
+    SELECT ebook_plus
+    FROM book_order)
     THEN
     SELECT price_ebook_plus + (price_inclusive_ebook - book_price.price_ebook)
     FROM book_price
-    WHERE book_price.book_id_id = (SELECT book_order.book_id_id FROM book_order WHERE id = last_insert_id())
-        INTO pricePerBook;
+    WHERE book_price.book_id_id =
+          (SELECT book_order.book_id_id FROM book_order WHERE id = last_insert_id()) INTO pricePerBook;
     ELSE
         IF (SELECT ebook FROM book_order)
         THEN
     SELECT price_inclusive_ebook
     FROM book_price
-    WHERE book_price.book_id_id = (SELECT book_order.book_id_id FROM book_order WHERE id = last_insert_id())
-        INTO pricePerBook;
+    WHERE book_price.book_id_id =
+          (SELECT book_order.book_id_id FROM book_order WHERE id = last_insert_id()) INTO pricePerBook;
     ELSE
     SELECT price_inclusive_ebook - book_price.price_ebook
     FROM book_price
-    WHERE book_price.book_id_id = (SELECT book_order.book_id_id FROM book_order WHERE id = last_insert_id())
-        INTO pricePerBook;
+    WHERE book_price.book_id_id =
+          (SELECT book_order.book_id_id FROM book_order WHERE id = last_insert_id()) INTO pricePerBook;
 END IF;
 END IF;
 
-    SET totalPrice = pricePerBook * (SELECT count FROM book_order WHERE id = last_insert_id());
+    SET
+totalPrice = pricePerBook * (SELECT count FROM book_order WHERE id = last_insert_id());
 
-UPDATE book_order SET price=totalPrice WHERE id = last_insert_id();
+UPDATE book_order
+SET price=totalPrice
+WHERE id = last_insert_id();
 
 UPDATE school_class
 SET used_budget=(totalPrice + (SELECT used_budget
