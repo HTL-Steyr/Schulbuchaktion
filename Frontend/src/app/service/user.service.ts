fbf083630303026f5e6270ae248245bf05a7dd73
s-prechtl <stefan@tague.at>
2023-03-23 10:54:09 +0100

    [TASK] login redirect and working (00:40h)

--- Frontend/src/app/service/user.service.ts
+++ Frontend/src/app/service/user.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {User} from "../model/user";
-import {Observable} from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { User } from "../model/user";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
-  constructor(private _http: HttpClient) { }
+    private _user?: User;
 
-  public findAll(): Observable<User[]> {
-    throw new Error("Method not implemented")
-  }
+    public get user(): User | undefined {
+        return this._user;
+    }
 
-  public findOneById(): Observable<User> {
-    throw new Error("Method not implemented")
-  }
+    public set user(value: User | undefined) {
+        if (!value) return;
+
+        this._user = value;
+    }
+
+    constructor(private _http: HttpClient) { }
+
+    public findAll(): Observable<User[]> {
+        throw new Error("Method not implemented")
+    }
+
+    public findOneById(): Observable<User> {
+        throw new Error("Method not implemented")
+    }
 }
