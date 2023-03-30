cb8209b405249c0eeaffe88a80f9701aed1c6d90
decker213 <decker@htl-steyr.ac.at>
2023-03-30 13:51:05 +0200

    [TASK] mysql-triggers.sql (02:00h)

--- Backend/app/sql/mysql-triggers.sql
+++ Backend/app/sql/mysql-triggers.sql
@@ -9,28 +9,28 @@ BEGIN
     DECLARE schoolClassId INTEGER;
     DECLARE departmentId INTEGER;
 
-    SELECT department_id_id FROM school_class WHERE school_class.id = NEW.school_class_id_id INTO departmentId;
+    SELECT department_id FROM school_class WHERE school_class.id = NEW.school_class_id INTO departmentId;
 
     SELECT price_inclusive_ebook - book_price.price_ebook
     FROM book_price
-    WHERE book_price.book_id_id =
-          NEW.book_id_id
+    WHERE book_price.book_id =
+          NEW.book_id
     INTO priceBook;
 
     IF (NEW.ebook_plus)
     THEN
         SELECT price_ebook_plus + (price_inclusive_ebook - book_price.price_ebook)
         FROM book_price
-        WHERE book_price.book_id_id =
-              NEW.book_id_id
+        WHERE book_price.book_id =
+              NEW.book_id
         INTO priceEbookBook;
     ELSE
         IF (NEW.ebook)
         THEN
             SELECT price_ebook
             FROM book_price
-            WHERE book_price.book_id_id =
-                  NEW.book_id_id
+            WHERE book_price.book_id =
+                  NEW.book_id
             INTO priceEbookBook;
         END IF;
     END IF;
@@ -41,13 +41,14 @@ BEGIN
     SET NEW.price = totalPrice;
 
     UPDATE school_class
-    SET used_budget=(totalPrice + NEW.school_class_id_id)
+    SET used_budget=(totalPrice + (SELECT used_budget
+                                   FROM school_class
+                                   WHERE school_class.id = schoolClassId))
     WHERE id = schoolClassId;
 
-    #UPDATE department
-    #SET used_budget=(totalPrice + (SELECT used_budget
-    #                             FROM department
-    #                              WHERE department.id = departmentId))
-    #WHERE id = departmentId;
+    UPDATE department
+    SET used_budget=(totalPrice + (SELECT used_budget
+                                   FROM department
+                                   WHERE department.id = departmentId))
+    WHERE id = departmentId;
 END;
-
