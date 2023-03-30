60a7c2667ed92e83416d2855b762b3d057b98d52
nicom <nmekina@htl-steyr.ac.at>
2023-03-30 16:22:53 +0200

    functions xlsx

--- Backend/app/src/Controller/ReadController.php
+++ Backend/app/src/Controller/ReadController.php
@@ -6,6 +6,7 @@ use App\Entity\Book;
 use App\Entity\BookPrice;
 use App\Entity\Publisher;
 use App\Entity\Subject;
+use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,16 +26,15 @@ class ReadController extends AbstractController
 
             $sheet = $spreadsheet->getSheet(0);
             for ($i = 2; $i <= $sheet->getHighestRow(); $i++) {
-                $vnr =  $sheet->getCell("J" . strval($i))->getValue();
-                $vname =  $sheet->getCell("K" . strval($i))->getValue();
+                $number = $sheet->getCell("J" . strval($i))->getValue();
+                $name = $sheet->getCell("K" . strval($i))->getValue();
 
-
-                $existing = $repo->findOneBy(["publisherNumber" => $vnr]);
+                $existing = $repo->findOneBy(["publisherNumber" => $number]);
 
                 if (!isset($existing)) {
                     $publisher = new Publisher();
-                    $publisher->setPublisherNumber($vnr);
-                    $publisher->setName($vname);
+                    $publisher->setPublisherNumber($number);
+                    $publisher->setName($name);
                     $repo->save($publisher, true);
                 }
             }
@@ -42,7 +42,6 @@ class ReadController extends AbstractController
         } else {
             die("datei existiert nicht!");
         }
-
 
         return $this->render('read/index.html.twig', [
             'controller_name' => 'ReadController',
@@ -52,7 +51,8 @@ class ReadController extends AbstractController
     #[Route('/read/xlsx/subject', name: 'app_read_subject')]
     public function readSubject(ManagerRegistry $registry): Response
     {
-        $repo = $registry->getRepository(Subject::class);
+        $repoSubject = $registry->getRepository(Subject::class);
+        $repoUser = $registry->getRepository(User::class);
 
         if (file_exists("Schulbuchliste_4100_2023_2024.xlsx")) {
             $reader = IOFactory::createReader("Xlsx");
@@ -60,15 +60,19 @@ class ReadController extends AbstractController
 
             $sheet = $spreadsheet->getSheet(0);
             for ($i = 2; $i <= $sheet->getHighestRow(); $i++) {
-                $vnr =  $sheet->getCell("J" . strval($i))->getValue();
+                $user = "cchimani";
+                $name = $sheet->getCell("F" . strval($i))->getValue();
+                $shortName = "AM";
 
-
-                $existing = $repo->findOneBy(["publisherNumber" => $vnr]);
+                $headOfSubjectId = $repoUser->findOneBy(["shortName" => $user]);
+                $existing = $repoSubject->findOneBy(["name" => $name]);
 
                 if (!isset($existing)) {
                     $subject = new Subject();
-                    $subject->setName($vnr);
-                    $repo->save($subject, true);
+                    $subject->setHeadOfSubject($headOfSubjectId);
+                    $subject->setName($name);
+                    $subject->setShortName($shortName);
+                    $repoSubject->save($subject, true);
                 }
             }
 
@@ -85,7 +89,9 @@ class ReadController extends AbstractController
     #[Route('/read/xlsx/book', name: 'app_read_book')]
     public function readBook(ManagerRegistry $registry): Response
     {
-        $repo = $registry->getRepository(Book::class);
+        $repoBook = $registry->getRepository(Book::class);
+        $repoSubject = $registry->getRepository(Subject::class);
+        $repoPublisher = $registry->getRepository(Publisher::class);
 
         if (file_exists("Schulbuchliste_4100_2023_2024.xlsx")) {
             $reader = IOFactory::createReader("Xlsx");
@@ -93,14 +99,38 @@ class ReadController extends AbstractController
 
             $sheet = $spreadsheet->getSheet(0);
             for ($i = 2; $i <= $sheet->getHighestRow(); $i++) {
-                $vnr =  $sheet->getCell("J" . strval($i))->getValue();
+                $subject =  $sheet->getCell("J" . strval($i))->getValue();
+                $publisher =  $sheet->getCell("K" . strval($i))->getValue();
+                $mainBook =  $sheet->getCell("L" . strval($i))->getValue();
+                $bookNumber =  $sheet->getCell("A" . strval($i))->getValue();
+                $title =  $sheet->getCell("C" . strval($i))->getValue();
+                $shortTitle =  $sheet->getCell("B" . strval($i))->getValue();
+                $listType =  $sheet->getCell("D" . strval($i))->getValue();
+                $schoolForm =  $sheet->getCell("E" . strval($i))->getValue();
+                $info =  $sheet->getCell("I" . strval($i))->getValue();
+                $ebook =  $sheet->getCell("P" . strval($i))->getValue();
+                $ebookPlus =  $sheet->getCell("Q" . strval($i))->getValue();
 
-                $existing = $repo->findOneBy(["publisherNumber" => $vnr]);
+                $subjectId = $repoSubject->findOneBy(["name" => $subject]);
+                $publisherId = $repoPublisher->findOneBy(["name" => $publisher]);
+                $mainBookId = $repoBook->findOneBy(["id" => $mainBook]);
+
+                $existing = $repoBook->findOneBy(["bookNumber" => $bookNumber]);
 
                 if (!isset($existing)) {
                     $book = new Book();
-                    $book->setPublisherNumber($vnr);
-                    $repo->save($book, true);
+                    $book->setSubject($subjectId);
+                    $book->setPublisher($publisherId);
+                    $book->setMainBook($mainBookId);
+                    $book->setBookNumber($bookNumber);
+                    $book->setTitle($title);
+                    $book->setShortTitle($shortTitle);
+                    $book->setListType($listType);
+                    $book->setSchoolForm($schoolForm);
+                    $book->setInfo($info);
+                    $book->setEbook($ebook);
+                    $book->setEbookPlus($ebookPlus);
+                    $repoBook->save($book, true);
                 }
             }
 
