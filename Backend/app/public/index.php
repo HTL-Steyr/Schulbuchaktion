b543b11d322c21d9b2ba84db4f3b389cbba2627a
prathgeb <rathgeb.peter@gmail.com>
2023-04-21 14:38:33 +0200

    Update index.php

--- Backend/app/public/index.php
+++ Backend/app/public/index.php
@@ -4,6 +4,9 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
