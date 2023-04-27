0650eb138622b98095fde0fb1383633d1f5ae583
David Hain <57726396+d-hain@users.noreply.github.com>
2023-04-27 11:14:08 +0200

    Update index.php

--- Backend/app/public/index.php
+++ Backend/app/public/index.php
@@ -6,6 +6,9 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+    die("success");
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
