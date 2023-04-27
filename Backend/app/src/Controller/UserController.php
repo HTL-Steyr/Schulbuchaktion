f8b748700a728df3d6e4b0f24aecfcebc91c7201
nschickm <nschickm@htl-steyr.ac.at>
2023-04-27 10:36:29 +0200

    [TASK] Added comments in UserController (00:50h)

--- Backend/app/src/Controller/UserController.php
+++ Backend/app/src/Controller/UserController.php
@@ -10,19 +10,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuilder;
 
+/*
+ * This controller is used to get the current user
+ */
+
 class UserController extends AbstractController
 {
+    /*
+     * @return Response -> the current user formatted as json
+     */
     #[Route('/user/getme', name: 'app_user')]
     public function getCurrentUser(AuthService $service, Request $request, ManagerRegistry $registry): Response
     {
+        //Get the current user
         $user = $service->authenticateByAuthorizationHeader($request);
         echo $user->getFirstName();
 
+        //Check if the user is logged in
         if ($user != null) {
-            $context=(new ObjectNormalizerContextBuilder())->withGroups('user')->toArray();
-
-            return $this->json($user,context:$context);
+            //Save the groups of which the content should be returned in the $context variable
+            $context = (new ObjectNormalizerContextBuilder())->withGroups('user')->toArray();
+            //Return the current user
+            return $this->json($user, context: $context);
         }
+        //Return HTTP UNAUTHORIZED if the user is not logged in or the token is invalid or expired or the user is not found
         return new Response(null, Response::HTTP_UNAUTHORIZED);
     }
 }
