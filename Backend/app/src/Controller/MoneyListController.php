59b848d663ce7667301588e5ae272ecd107e4124
David Hain <57726396+d-hain@users.noreply.github.com>
2023-05-11 14:08:25 +0200

    [BUGFIX] added /moneylist route (00:05h)

--- Backend/app/src/Controller/MoneyListController.php
+++ Backend/app/src/Controller/MoneyListController.php
@@ -19,21 +19,21 @@ use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuild
 class MoneyListController extends AbstractController {
 
     /**
-     * This method returns the moneylist with the given book id.
+     * This method returns the moneylist with the given id.
      *
      * @param AuthService $authService - an instance of the AuthService service class for authentication
      * @param Request $request - the incoming HTTP request
      * @param ManagerRegistry $registry - an instance of the Doctrine ManagerRegistry service class for database management
-     * @param int $id - the id of the book for which to retrieve the moneylist
+     * @param int $id - the id of the moneylist for which to retrieve the moneylist
      *
      * @return Response - a JSON response containing the moneylist object or a null value with an appropriate status code
      */
     #[Route(
         path: "/moneylist/{id}",
-        name: "app_moneylist_get_by_book_id",
+        name: "app_moneylist_get",
         methods: ["GET"]
     )]
-    public function getMoneyListByBookId(AuthService $authService, Request $request, ManagerRegistry $registry, int $id): Response {
+    public function getMoneyListById(AuthService $authService, Request $request, ManagerRegistry $registry, int $id): Response {
         //Get the current user
         $user = $authService->authenticateByAuthorizationHeader($request);
         //Check if the user is logged in
@@ -55,6 +55,39 @@ class MoneyListController extends AbstractController {
                 // Return a JSON response with the BookPrice entity and the specified serialization groups
                 return $this->json($moneylist, status: Response::HTTP_OK, context: $context);
 
+            }
+        }
+
+        // Return a null JSON response with a NOT_FOUND status code if the BookPrice entity is not found
+        return $this->json(null, status: Response::HTTP_NOT_FOUND);
+    }
+    
+    #[Route(
+        path: "/moneylist",
+        name: "app_moneylist_get_all",
+        methods: ["GET"]
+    )]
+    public function getMoneyLists(AuthService $authService, Request $request, ManagerRegistry $registry): Response {
+        //Get the current user
+        $user = $authService->authenticateByAuthorizationHeader($request);
+        //Check if the user is logged in
+        if (!isset($user)) {
+            //Return HTTP UNAUTHORIZED if the user is not logged in or the token is invalid or expired or the user is not found
+            return new Response(null, Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($user->getRole()->getName() == "Admin" || $user->getRole()->getName() == "Abteilungsvorstand") {
+            // Create a context object for the ObjectNormalizer that specifies the serialization groups to use
+            $context = (new ObjectNormalizerContextBuilder())
+                ->withGroups("bookPrice")
+                ->toArray();
+
+            // Get the BookPrice entity with the given book id from the database
+            $moneylist = $registry->getRepository(BookPrice::class)->findAll();
+
+            if (isset($moneylist)) {
+                // Return a JSON response with the BookPrice entity and the specified serialization groups
+                return $this->json($moneylist, status: Response::HTTP_OK, context: $context);
             }
         }
 
