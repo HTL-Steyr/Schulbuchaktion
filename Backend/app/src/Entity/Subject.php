97e9145b679eacfd108ac60ad4d883819f9eb807
David Hain <57726396+d-hain@users.noreply.github.com>
2023-05-11 15:59:30 +0200

    [TASK] added bookPrice group (00:10h)

--- Backend/app/src/Entity/Subject.php
+++ Backend/app/src/Entity/Subject.php
@@ -10,17 +10,17 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: SubjectRepository::class)]
 class Subject {
-    #[Groups(['subject', "orderlist"])]
+    #[Groups(['subject', "orderlist", "bookPrice"])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[Groups(['subject', "orderlist"])]
+    #[Groups(['subject', "orderlist", "bookPrice"])]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[Groups(['subject', "orderlist"])]
+    #[Groups(['subject', "orderlist", "bookPrice"])]
     #[ORM\Column(length: 255)]
     private ?string $shortName = null;
 
