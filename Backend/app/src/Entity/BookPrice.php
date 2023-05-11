cb0d39930a3997e6569b74fcef15d235c1aac46d
David Hain <57726396+d-hain@users.noreply.github.com>
2023-05-11 14:40:41 +0200

    [TASK] added orderlist group (00:10h)

--- Backend/app/src/Entity/BookPrice.php
+++ Backend/app/src/Entity/BookPrice.php
@@ -8,25 +8,25 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BookPriceRepository::class)]
 class BookPrice {
-    #[Groups(['subject', 'bookPrice'])]
+    #[Groups(['subject', 'bookPrice', "orderlist"])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[Groups(['subject', 'bookPrice'])]
+    #[Groups(['subject', 'bookPrice', "orderlist"])]
     #[ORM\Column]
     private ?int $year = null;
 
-    #[Groups(['subject', 'bookPrice'])]
+    #[Groups(['subject', 'bookPrice', "orderlist"])]
     #[ORM\Column]
     private ?int $priceInclusiveEbook = null;
 
-    #[Groups(['subject', 'bookPrice'])]
+    #[Groups(['subject', 'bookPrice', "orderlist"])]
     #[ORM\Column(nullable: true)]
     private ?int $priceEbook = null;
 
-    #[Groups(['subject', 'bookPrice'])]
+    #[Groups(['subject', 'bookPrice', "orderlist"])]
     #[ORM\Column(nullable: true)]
     private ?int $priceEbookPlus = null;
 
