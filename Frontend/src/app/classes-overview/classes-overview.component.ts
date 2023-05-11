858acb3cf4aad3ae398a319552a2f8ad02f46f02
jovana <jmuzdeka@htl-steyr.ac.at>
2023-05-11 13:55:09 +0200

    [TASK] added and binded mockdata to datagrid in classes-overview.component.html and classes-overview.component.ts (01:30h)

--- Frontend/src/app/classes-overview/classes-overview.component.ts
+++ Frontend/src/app/classes-overview/classes-overview.component.ts
@@ -12,4 +12,22 @@ export class ClassesOverviewComponent {
     {field: '4AHTIN'},
     {field: '5AHITN'}
   ];
+
+  public classes = [
+    { JG: '2019', Klasse: '3AHITN', 'SuS ඞ': 'Max Mustermann', HEL: 2, HME: true },
+    { JG: '2020', Klasse: '3AHITN', 'SuS ඞ': 'Maria Musterfrau', HEL: 3, HME: false },
+    { JG: '2019', Klasse: '4AHTIN', 'SuS ඞ': 'John Doe', HEL: 4, HME: true },
+    { JG: '2020', Klasse: '4AHTIN', 'SuS ඞ': 'Jane Doe', HEL: 5, HME: false },
+    { JG: '2019', Klasse: '5AHITN', 'SuS ඞ': 'Hans Huber', HEL: 6, HME: true },
+    { JG: '2020', Klasse: '5AHITN', 'SuS ඞ': 'Lisa Müller', HEL: 7, HME: false },
+  ];
+  selectedClass: string | null = null;
+  filteredClasses: any[] = [];
+
+
+  selectClass(className: string) {
+    this.selectedClass = className;
+    this.filteredClasses = this.classes.filter((item) => item.Klasse == className);
+
+  }
 }
