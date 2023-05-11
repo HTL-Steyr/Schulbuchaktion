07dc60436a24bcaa0be084d62d6719ab73e652dc
David Hain <57726396+d-hain@users.noreply.github.com>
2023-05-11 16:24:12 +0200

    [TASK] removed bookPrice where unnecessary (00:10h)

--- Backend/app/src/Entity/Publisher.php
+++ Backend/app/src/Entity/Publisher.php
@@ -10,17 +10,17 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PublisherRepository::class)]
 class Publisher {
-    #[Groups(["orderlist", "bookPrice"])]
+    #[Groups(["orderlist"])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
     
-    #[Groups(["orderlist", "bookPrice"])]
+    #[Groups(["orderlist"])]
     #[ORM\Column]
     private ?int $publisherNumber = null;
     
-    #[Groups(["orderlist", "bookPrice"])]
+    #[Groups(["orderlist"])]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
