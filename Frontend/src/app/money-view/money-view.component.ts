4122004382e9da6d4cfc8803573e76fd721c25c8
lnagler1 <lnagler1@htl-steyr.ac.at>
2023-05-14 01:07:04 +0200

    [TASK] money-view, editing (not yet in sync with backend), paging, grouping (3:00h)

--- Frontend/src/app/money-view/money-view.component.ts
+++ Frontend/src/app/money-view/money-view.component.ts
@@ -1,8 +1,4 @@
 import {Component, NgModule, OnInit} from '@angular/core';
-import {Observable} from "rxjs";
-import {MoneylistEntry} from "../model/moneylistEntry";
-import DevExpress from "devextreme";
-import CustomStore = DevExpress.data.CustomStore;
 import { Datasource } from '../datasources/datasource';
 import { MoneylistService } from '../service/moneylist.service';
 
