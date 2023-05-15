b655e78773e14a5a5d71afa006da422d6d9c2893
David Hain <57726396+d-hain@users.noreply.github.com>
2023-05-15 14:20:08 +0200

    [BUFGIFX] added import (00:05h)

--- Backend/app/src/Controller/BookController.php
+++ Backend/app/src/Controller/BookController.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Service\AuthService;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuilder;
