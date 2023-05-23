46b983a607d42b45f493ab1c5af12597dd21ce56
decker213 <decker@htl-steyr.ac.at>
2023-05-23 09:26:30 +0200

    [TASK] clean up SchoolClassController.php

--- Backend/app/src/Controller/SchoolClassController.php
+++ Backend/app/src/Controller/SchoolClassController.php
@@ -14,40 +14,37 @@ use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuild
 /*
  * This controller is either used to get all schoolclasses or a schoolclass by id
  */
-class SchoolClassController extends AbstractController {
+
+class SchoolClassController extends AbstractController
+{
     /**
      * @return Response -> all schoolclasses
      */
 
     #[Route(
-        path: "/schoolclass",
-        name: "app_schoolclass",
-        methods: ["GET"]
+        path: '/schoolclass',
+        name: 'app_schoolclass',
+        methods: ['GET']
     )]
-    public function getSchoolClasses(AuthService $authService, Request $request, ManagerRegistry $registry): Response {
-        //Get the current user
-        $user = $authService->authenticateByAuthorizationHeader($request);
-        //Check if the user is logged in
-        if (!isset($user)) {
-            //Return HTTP UNAUTHORIZED if the user is not logged in or the token is invalid or expired or the user is not found
-            return new Response(null, Response::HTTP_UNAUTHORIZED);
-        }
-        
-        if ($user->getRole()->getName() == "Admin") {
-            // Build a context to normalize the data with
-            $context = (new ObjectNormalizerContextBuilder())
-                ->withGroups('schoolclass')
-                ->toArray();
-            
+    public function getSchoolClasses(AuthService $authService, Request $request, ManagerRegistry $registry): Response
+    {
+        // Authenticate the user using the Authorization header
+        if ($authService->authenticateByAuthorizationHeader($request)->getRole()->getName() == "Admin") {
+            $user = $authService->authenticateByAuthorizationHeader($request);
+            // If the user is not authenticated, return an HTTP_UNAUTHORIZED response
+            if (!isset($user)) {
+                return new Response(null, Response::HTTP_UNAUTHORIZED);
+            }
             // Find all school classes from the repository
             $schoolClasses = $registry->getRepository(SchoolClass::class)->findAll();
-            
             // If there are any school classes, return them with an HTTP_OK response and the built context
             if (isset($schoolClasses)) {
-                return $this->json($schoolClasses, status: Response::HTTP_OK, context: $context);
+                return new Response(json_encode($schoolClasses), Response::HTTP_OK);
             }
+        } else {
+            // If the user is not authenticated, return an HTTP_UNAUTHORIZED response
+            return new Response(null, Response::HTTP_UNAUTHORIZED);
         }
-        
         // If no school classes are found, return an HTTP_NOT_FOUND response
         return $this->json(null, status: Response::HTTP_NOT_FOUND);
     }
@@ -56,35 +53,29 @@ class SchoolClassController extends AbstractController {
      * @return Response -> the schoolclass with the given id
      */
     #[Route(
-        path: "/schoolclass/{id}",
-        name: "app_schoolclass_get_by_id",
-        methods: ["GET"]
+        path: '/schoolclass/{id}',
+        name: 'app_schoolclass_get_by_id',
+        methods: ['GET']
     )]
-    public function getSchoolClassById(AuthService $authService, Request $request, ManagerRegistry $registry, int $id): Response {
-        //Get the current user
-        $user = $authService->authenticateByAuthorizationHeader($request);
-        //Check if the user is logged in
-        if (!isset($user)) {
-            //Return HTTP UNAUTHORIZED if the user is not logged in or the token is invalid or expired or the user is not found
-            return new Response(null, Response::HTTP_UNAUTHORIZED);
-        }
-        
+    public function getSchoolClassById(AuthService $authService, Request $request, ManagerRegistry $registry, int $id): Response
+    {
         // Authenticate the user using the Authorization header
-        if ($user->getRole()->getName() == "Admin") {
-            // Build a context to normalize the data with
-            $context = (new ObjectNormalizerContextBuilder())
-                ->withGroups('schoolclass')
-                ->toArray();
-            
+        if ($authService->authenticateByAuthorizationHeader($request)->getRole()->getName() == "Admin") {
+            $user = $authService->authenticateByAuthorizationHeader($request);
+            // If the user is not authenticated, return an HTTP_UNAUTHORIZED response
+            if (!isset($user)) {
+                return new Response(null, Response::HTTP_UNAUTHORIZED);
+            }
             // Find the school class with the given id from the repository
             $schoolClass = $registry->getRepository(SchoolClass::class)->find($id);
-            
             // If the school class is found, return it with an HTTP_OK response and the built context
             if (isset($schoolClass)) {
-                return $this->json($schoolClass, status: Response::HTTP_OK, context: $context);
+                return new Response(json_encode($schoolClass), Response::HTTP_OK);
             }
+        } else {
+            // If the user is not authenticated, return an HTTP_UNAUTHORIZED response
+            return new Response(null, Response::HTTP_UNAUTHORIZED);
         }
-        
         // If the school class is not found, return an HTTP_NOT_FOUND response
         return $this->json(null, status: Response::HTTP_NOT_FOUND);
     }
