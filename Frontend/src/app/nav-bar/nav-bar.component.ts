86fd555cbdba8c02a32f221da96e153a63161d02
Daniel <dwimmer1@htl-steyr.ac.at>
2023-06-01 15:51:45 +0200

    [TASK] NavBar Test

--- Frontend/src/app/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { UserService } from '../service/user.service';
+import {Component} from '@angular/core';
+import {UserService} from '../service/user.service';
 import {User} from "../model/user";
 import {Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-nav-bar',
@@ -13,16 +14,27 @@ export class NavBarComponent {
   private currentUser: User | undefined;
   isDisabledMoney: boolean = true;
   isDisabledClass: boolean = true;
-  constructor(public userService: UserService, private router:Router) {
+  isDisabledImports: boolean = true;
 
-        if (userService.user?.role.id==1||userService.user?.role.id==2) {
-          this.isDisabledMoney=false
-       }
-         if (userService.user?.role.id==1){
-         this.isDisabledClass=false
-       }
+  // <a routerLink="/import" *ngIf="isDisabledClass">Import</a>
+  constructor(public userService: UserService, private router: Router) {
 
-     }
+    if (userService.user?.role.id == 1 || userService.user?.role.id == 2) {
+
+      this.isDisabledMoney = false
+    }
+    if (userService.user?.role.id == 1) {
+      this.isDisabledClass = false
+    }
+
+
+    if (userService.user?.role.id == 3) {
+      this.isDisabledClass = false
+      this.isDisabledImports = true;
+      this.isDisabledMoney = false
+    }
+
+  }
 
   logoutButtonClicked() {
     this.userService.logout();
