eddc90ae36c1e2ce3f9493e25b62924c93dd7fb8
Daniel <dwimmer1@htl-steyr.ac.at>
2023-06-10 10:40:56 +0200

    [TASK] Disable Menue Items [02:30]

--- Frontend/src/app/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -2,48 +2,71 @@ import {Component} from '@angular/core';
 import {UserService} from '../service/user.service';
 import {User} from "../model/user";
 import {Router} from "@angular/router";
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'showMenuItem',})
 
 
+export class ShowMenuItemPipe implements PipeTransform {
+  transform(menuItemName: string, role: string): boolean {
+    if (menuItemName === 'Import') {
+      return role === '1';
+    } else if (menuItemName === 'Bestelluebersicht') {
+      return role === '1' || role === '2' || role === '3' || role === '4';
+    } else if (menuItemName === 'Klassenuebersicht') {
+      return role === '1';
+    } else if (menuItemName === 'Gelduebersicht') {
+      return role === '1' || role === '2';
+    }
+    return true;
+  }
+}
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
+
 })
 export class NavBarComponent {
-
+  /*
   private currentUser: User | undefined;
   isDisabledMoney: boolean = true
   isDisabledClass: boolean = true
   isDisabledImports: boolean = true
-
-  // <a routerLink="/import" *ngIf="isDisabledClass">Import</a>
+   */
   constructor(public userService: UserService, private router: Router) {
 
-console.log("construct")
-    if (userService.user?.role.id == 1 || userService.user?.role.id == 2) {
-
-      this.isDisabledMoney = false
-      this.isDisabledImports = false
-      this.isDisabledClass = false
-
-
-    } else if (userService.user?.role.id == 3) {
-      this.isDisabledClass = true
-      this.isDisabledImports = true;
-      this.isDisabledMoney = true
-
-    } else if (userService.user?.role.id == 4) {
-      this.isDisabledClass = true
-      this.isDisabledImports = true;
-      this.isDisabledMoney = true
-    }
   }
-
-  configureRights(userService: UserService, router: Router) {
-
-  }
-
-
+  navItems = [
+    {
+      name: 'Bestelluebersicht',
+      path: '/bestelluebersicht',
+      text: 'Bestelluebersicht',
+      icon: '',
+      role: '3'
+    },
+    {
+      name: 'Klassenuebersicht',
+      path: '/klassenuebersicht',
+      text: 'Klassenuebersicht',
+      icon: '',
+      role: '4'
+    },
+    {
+      name: 'Gelduebersicht',
+      path: '/gelduebersicht',
+      text: 'Gelduebersicht',
+      icon: '',
+      role: '4'
+    },
+    {
+      name: 'Import',
+      path: '/import',
+      text: 'Import',
+      icon: '',
+      role: '4'
+    },
+  ];
   logoutButtonClicked() {
     this.userService.logout();
     this.router.navigate(['/login']);
