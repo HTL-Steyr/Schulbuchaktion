3e3e775c919e6e51878c6a1acab9d8a36215ad99
sbegic03 <sbegic@htl-steyr.ac.at>
2023-06-13 10:45:30 +0200

    [Xls_Import]Preview of file(02:00)

--- Frontend/src/app/upload-button/upload-button.component.ts
+++ Frontend/src/app/upload-button/upload-button.component.ts
@@ -32,14 +32,14 @@ export class UploadButtonComponent {
       const ws: XLSX.WorkSheet = wb.Sheets[wsname];
 
       /* save data */
-      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1, raw: false, range: 10}));
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1, raw: false, range: 0}));
       console.log(this.data[1]);
 
       this.headData = this.data[0];
       this.data = this.data.slice(1); // remove first header record
 
       const ws2: XLSX.WorkSheet = wb.Sheets[wb.SheetNames[1]];
-      this.readDataSheet(ws2, 10);
+      this.readDataSheet(ws2, 0);
     };
     reader.readAsBinaryString(target.files[0]);
   }
@@ -49,12 +49,24 @@ export class UploadButtonComponent {
     let datas = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1, raw: false, range: startRow}));
     console.log(datas[1]);
     let headDatas = datas[0];
-    datas = datas.slice(1); // remove first header record
+    // datas = datas.slice(1); // remove first header record
 
     for (let i = 0; i < this.data.length; i++) {
       this.data[i][this.headData.length] = datas.filter(x => x[12] == this.data[i][0])
     }
     console.log(this.data[1]);
+  }
+
+  import(): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
   }
 
   export(): void {
